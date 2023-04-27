4c1f228643eea9a0179c4ec6dec521eee719f2e5
Hashimjasem <j_hashim1305@outlook.com>
2023-04-27 22:58:56 +0800

    navbar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 function NavBar() {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
     return (
         <div className="flex justify-between items-center mx-auto max-w-[1240px] px-4 text-white h-24 ">
-            <h1 className="w-full text-3xl font-bold text-[#00df9a]">Hashim</h1>
+            <h1 className="w-full text-3xl font-bold text-[#00df9a]">HASHIM JASEM</h1>
             <ul className="flex hidden">
                 <li className="p-4">home</li>
                 <li className="p-4">about me</li>
@@ -12,18 +18,19 @@ function NavBar() {
                 <li className="p-4">Blogs</li>
                 <li className="p-4">Contact Me</li>
             </ul>
+
             <div>
                 <AiOutlineMenu size={20} />
             </div>
-        
-            <div className="fixed left-0 top-0 w-[60%] h-full bg-[#000300] border-r border-r-gray-900">
-            <h1 className="w-full text-3xl font-bold text-[#00df9a]">Hashim</h1>
 
-                <ul className="pt-24 uppercase">
-                    <li className="p-4">home</li>
-                    <li className="p-4">about me</li>
-                    <li className="p-4">projects</li>
-                    <li className="p-4">Blogs</li>
+            <div className="fixed left-0 top-0 w-[60%] h-full bg-[#000300] border-r border-r-gray-900">
+                <h1 className="w-full text-3xl font-bold text-[#00df9a]">HASHIM JASEM</h1>
+
+                <ul className="uppercase p-4">
+                    <li className="p-4 border-b border-gray-600">Blogs</li>
+                    <li className="p-4 border-b border-gray-600">home</li>
+                    <li className="p-4 border-b border-gray-600">about me</li>
+                    <li className="p-4 border-b border-gray-600">projects</li>
                     <li className="p-4">Contact Me</li>
                 </ul>
             </div>
