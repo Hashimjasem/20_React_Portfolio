903fa8253d0420d514095a43d0542dc4a24408ab
Hashimjasem <j_hashim1305@outlook.com>
2023-04-30 21:55:02 +0800

    newsletter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import NavBar from "./components/navBar";
 import Analytics from "./components/analytics";
+import Newsletter from "./components/newsletter";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar/>
       <Hero/>
       <Analytics/>
+      <Newsletter/>
     </div>
   );
 }
