6e2be45966629f6cb6c70cab12db19e72fd74e6d
Hashimjasem <j_hashim1305@outlook.com>
2023-05-16 16:29:26 +0800

    fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Hero from "./components/Hero";
 import NavBar from "./components/navBar";
 import Skills from "./components/skills";
-import Newsletter from "./components/newsletter";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <NavBar/>
       <Hero/>
       <Skills/>
-      <Newsletter/>
+      <Projects/>
+      <Contact/>
     </div>
   );
 }
